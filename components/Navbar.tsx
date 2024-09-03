c69d274e84260bd295c335b5bcd3da2a495c5a42
ahmedsheikh27 <932ahmedsheikh@gmail.com>
2024-09-03 23:34:37 +0500

    added navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+const Navbar = () => {
+    return (
+        <div className='bg-gray-100 py-3 w-full flex '>
+            <div className='items-center flex '>
+                <Link href="/sign-up"
+                className='items-center flex '
+                >
+                <Image src='/navlogo.jfif' alt=''
+                    className='bg-transparent rounded-lg ml-2'
+                    width={30}
+                    height={30} />
+                <h1 className='font-mono text-[30px] font-bold text-red-600'>EasyBank</h1>
+                    </Link>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
