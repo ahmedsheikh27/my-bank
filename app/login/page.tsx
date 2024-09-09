a35f77f44bc657e85f2101886c62957a70f6b9ed
ahmedsheikh27 <932ahmedsheikh@gmail.com>
2024-09-09 19:43:15 +0500

    page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,103 +1,103 @@
-'use client'
-import Link from 'next/link';
-import React, { useEffect, useState } from 'react'
+// 'use client'
+// import Link from 'next/link';
+// import React, { useEffect, useState } from 'react'
 
-export const page = () => {
-    const [user, setUser] = useState({
-        email: '',
-        password: '',
-        name: '',
-    });
-    const [message, setMessage] = useState('');
-    const [oldUser, setOldUser] = useState<null | { email: string; name: string }>(null);
+// export const page = () => {
+//     const [user, setUser] = useState({
+//         email: '',
+//         password: '',
+//         name: '',
+//     });
+//     const [message, setMessage] = useState('');
+//     const [oldUser, setOldUser] = useState<null | { email: string; name: string }>(null);
 
-    useEffect(() => {
-        const storedUser = localStorage.getItem('user');
-        if (storedUser) {
-            setOldUser(JSON.parse(storedUser));
-        }
-    }, []); // Add dependency array to run only once on mount
+//     useEffect(() => {
+//         const storedUser = localStorage.getItem('user');
+//         if (storedUser) {
+//             setOldUser(JSON.parse(storedUser));
+//         }
+//     }, []); // Add dependency array to run only once on mount
 
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setUser({ ...user, [event.target.name]: event.target.value });
-    };
+//     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+//         setUser({ ...user, [event.target.name]: event.target.value });
+//     };
 
-    const handleLogin = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const { email, name, password } = user;
-        if (!email || !password || !name) {
-            setMessage('Please fill all inputs');
-            alert('Please fill all inputs');
-        } else {
-            const newUser = localStorage.getItem('user');
-            if (newUser) {
-                const objUser = JSON.parse(newUser);
-                if (objUser.email === email) {
-                    setMessage('Email already exists');
-                    alert(message);
-                }
-            }
-            localStorage.setItem('user', JSON.stringify(user));
-            setOldUser(user);
-            setMessage('User is signed up');
-            alert('User is signed up');
-        }
-        window.location.href = '/'
-    };
+//     const handleLogin = (event: React.FormEvent<HTMLFormElement>) => {
+//         event.preventDefault();
+//         const { email, name, password } = user;
+//         if (!email || !password || !name) {
+//             setMessage('Please fill all inputs');
+//             alert('Please fill all inputs');
+//         } else {
+//             const newUser = localStorage.getItem('user');
+//             if (newUser) {
+//                 const objUser = JSON.parse(newUser);
+//                 if (objUser.email === email) {
+//                     setMessage('Email already exists');
+//                     alert(message);
+//                 }
+//             }
+//             localStorage.setItem('user', JSON.stringify(user));
+//             setOldUser(user);
+//             setMessage('User is signed up');
+//             alert('User is signed up');
+//         }
+//         window.location.href = '/'
+//     };
 
-    // const signOut = () => {
-    //     setOldUser(null)
-    //     // localStorage.removeItem('user')
-    //     alert('User is Signed Out')
-    // }
+//     // const signOut = () => {
+//     //     setOldUser(null)
+//     //     // localStorage.removeItem('user')
+//     //     alert('User is Signed Out')
+//     // }
 
-    return (
-       <>
-                <div className='h-screen w-full bg-white items-center flex justify-center py-3'>
-        {oldUser? 
+//     return (
+//        <>
+//                 <div className='h-screen w-full bg-white items-center flex justify-center py-3'>
+//         {oldUser? 
         
-            <div>
-                <h2>Welcome, {oldUser.name}!</h2>
-                <p>Your email: {oldUser.email}</p>
-                {/* <button
-                onClick={signOut}
-                type='submit'
-                className='w-[220px] px-3 h-[40px] 0 rounded-2xl bg-blue-600 text-white  border-none mt-3 '>
-                    SignOut</button> */}
+//             <div>
+//                 <h2>Welcome, {oldUser.name}!</h2>
+//                 <p>Your email: {oldUser.email}</p>
+//                 {/* <button
+//                 onClick={signOut}
+//                 type='submit'
+//                 className='w-[220px] px-3 h-[40px] 0 rounded-2xl bg-blue-600 text-white  border-none mt-3 '>
+//                     SignOut</button> */}
                 
-            </div>
-          :
+//             </div>
+//           :
           
-          <form onSubmit={handleLogin}>
-                <div className='w-80 h-auto bg-white items-center m-3 flex flex-col p-3 rounded-md border-gray-300  border-solid border-[1px]'>
-                    <h1>
-                        Sign In
-                    </h1>
-                    <div className='gap-5 mt-5 m-3  flex flex-col justify-between  '>
-                        <label> Name:
-                            <input type="text" placeholder='name' name='name' onChange={handleChange} className='w-[300px] px-3 h-[50px] 0 rounded-md border-black  border-solid border-[1px]  ' />
-                        </label>
-                        <label> Email:
-                            <input type="email" placeholder='your@gmail.com' name='email' onChange={handleChange} className='w-[300px] px-3 h-[50px]  rounded-md border-black  border-solid border-[1px] ' />
-                        </label>
-                        <label>Password:
-                            <input type="password" placeholder='password' name='password' onChange={handleChange} className='w-[300px] px-3 h-[50px]  rounded-md border-black  border-solid border-[1px] ' />
-                        </label>
-                    </div>
+//           <form onSubmit={handleLogin}>
+//                 <div className='w-80 h-auto bg-white items-center m-3 flex flex-col p-3 rounded-md border-gray-300  border-solid border-[1px]'>
+//                     <h1>
+//                         Sign In
+//                     </h1>
+//                     <div className='gap-5 mt-5 m-3  flex flex-col justify-between  '>
+//                         <label> Name:
+//                             <input type="text" placeholder='name' name='name' onChange={handleChange} className='w-[300px] px-3 h-[50px] 0 rounded-md border-black  border-solid border-[1px]  ' />
+//                         </label>
+//                         <label> Email:
+//                             <input type="email" placeholder='your@gmail.com' name='email' onChange={handleChange} className='w-[300px] px-3 h-[50px]  rounded-md border-black  border-solid border-[1px] ' />
+//                         </label>
+//                         <label>Password:
+//                             <input type="password" placeholder='password' name='password' onChange={handleChange} className='w-[300px] px-3 h-[50px]  rounded-md border-black  border-solid border-[1px] ' />
+//                         </label>
+//                     </div>
 
-                    <button
-                        type='submit'
-                        className='w-[220px] px-3 h-[40px] 0 rounded-2xl bg-blue-600 text-white  border-none mt-3 '>Log In</button>
-<div className='flex mt-6'>
-<p>Don't have an account:</p>
-<Link href='/signup' className='text-blue-600'>SignUp</Link>
-<p>here</p>
-</div>
-                </div>
-            </form>
-        }
-        </div>
-        </>
-    )
-}
-export default page
+//                     <button
+//                         type='submit'
+//                         className='w-[220px] px-3 h-[40px] 0 rounded-2xl bg-blue-600 text-white  border-none mt-3 '>Log In</button>
+// <div className='flex mt-6'>
+// <p>Don't have an account:</p>
+// <Link href='/signup' className='text-blue-600'>SignUp</Link>
+// <p>here</p>
+// </div>
+//                 </div>
+//             </form>
+//         }
+//         </div>
+//         </>
+//     )
+// }
+// export default page
